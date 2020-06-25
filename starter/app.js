68c1828f6d53a070a4ef48ff3e7d7986f02f82ea
Connor Burgess <cburgess4049@gmail.com>
2020-06-25 13:04:41 -0500

    Budgeting App Architecture

--- starter/app.js
+++ starter/app.js
@@ -1,2 +1,39 @@
 //Budgeting Application
-//test
+
+var budgetController = (function () {
+    var x = 23;
+    
+    var add = function(a) {
+        return x + a;
+    }
+    return {
+        publicTest: function(b) {
+            return add(b);
+        }
+    }
+})();
+
+
+
+
+
+
+var UIController = (function() {
+    
+    //Code for the UI controller
+    
+    
+})();
+
+
+//Connecting controller.  Knows both the budget and UI controller so it can bridge them
+var controller = (function(bdgtCtrl, UICtrl) {
+    var z = bdgtCtrl.publicTest(20);
+    
+    return {
+        anotherPublic: function() {
+            console.log(z);
+        }
+    }
+    
+})(budgetController, UIController);
